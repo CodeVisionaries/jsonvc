3e44407d154288b9809274fd81f8e13a526d1b9b
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-03-28 07:47:50 +0100

    eliminate reliance on `compute_json_hash` in `ExtJsonPatch` and `JsonGraphNode` classes

--- src/jsontools/json/models.py
+++ src/jsontools/json/models.py
@@ -1,10 +1,13 @@
-from typing import List
+from typing import (
+    List,
+    Callable,
+    Optional,
+)
 from .base_models import (
     ExtJsonPatchBase,
     JsonGraphNodeBase,
 )
 from ..jsonpatch_ext import apply_ext_patch
-from ..checksum import compute_json_hash
 
 
 def _none_to_list(obj):
@@ -12,31 +15,34 @@ def _none_to_list(obj):
 
 
 def _none_to_dict(obj):
-    return obj if obj is not None else {} 
+    return obj if obj is not None else {}
 
 
 class ExtJsonPatch:
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args, hash_func: Optional[Callable]=None, **kwargs) -> None:
         source_hashes = kwargs.get('sourceHashes', None)
         if source_hashes is not None:
-            # normalization: put keys into leixcographic order 
+            # normalization: put keys into leixcographic order
             sorted_dict = {}
             for k in sorted(source_hashes):
-                sorted_dict[k] = source_hashes[k] 
+                sorted_dict[k] = source_hashes[k]
             kwargs['sourceHashes'] = sorted_dict
         self._datamodel = ExtJsonPatchBase(*args, **kwargs)
+        self._hash_func = hash_func
 
     def __hash__(self) -> int:
         return int(self.get_hash(), 16)
 
     def get_hash(self) -> str:
-        return compute_json_hash(self.model_dump())
+        if self._hash_func is None:
+            raise TypeError('Initialize this class with the `hash_func` argument')
+        return self._hash_func(self.model_dump())
 
     def get_source_hashes(self) -> list:
         return list(self._datamodel.sourceHashes.values())
 
-    def apply(self, load_json_func: callable):
+    def apply(self, load_json_func: Callable):
         json_dict = self.model_dump()
         return apply_ext_patch(json_dict, load_json_func)
 
@@ -46,13 +52,14 @@ class ExtJsonPatch:
 
 class JsonGraphNode:
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args, hash_func: Optional[Callable]=None, **kwargs) -> None:
         source_hashes = kwargs['sourceHashes']
         if source_hashes is not None:
             # normalization: sort source node hashes lexicographically
             source_node_hashes = sorted(set(source_hashes))
             kwargs['sourceHashes'] = source_hashes
         self._datamodel = JsonGraphNodeBase(*args, **kwargs)
+        self._hash_func = hash_func
 
     def get_ext_patch_hash(self) -> str:
         return self._datamodel.extJsonPatchHash
@@ -63,10 +70,12 @@ class JsonGraphNode:
     def __hash__(self) -> int:
         # NOTE: calling hash(obj) truncates the result
         #       compared to calling obj.__hash__()
-        return int(self.get_hash(), 16) 
+        return int(self.get_hash(), 16)
 
     def get_hash(self) -> str:
-        return compute_json_hash(self.model_dump())
+        if self._hash_func is None:
+            raise TypeError('Initialize this class with the `hash_func` argument')
+        return self._hash_func(self.model_dump())
 
     def get_document_hash(self) -> List[str]:
         return self._datamodel.documentHash
