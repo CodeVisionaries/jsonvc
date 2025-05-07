0aa00b6b9ce079bc82fb666ff13d6cdb30894f5b
gschnabel <40870991+gschnabel@users.noreply.github.com>
2025-05-07 08:38:27 +0200

    implement `IpfsJsonStorageProvider` class

---
+++ src/jsontools/ipfs_storage.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from . import ipfs_storage_utils as ipfs_jsu
+from .storage import (
+    JsonStorageProvider,
+    JsonObjectIndex,
+)
+from pathlib import Path
+
+
+class IpfsJsonStorageProvider(JsonStorageProvider):
+
+    def __init__(self, cache_dir: Path, gateway_url: str, rpc_api_url: str):
+        self._cache_dir = Path(cache_dir)
+        self._gateway_url = gateway_url
+        self._rpc_api_url = rpc_api_url
+        self._cache_dir = Path(cache_dir)
+
+    def load(self, json_hash: str) -> dict:
+        if ipfs_jsu.exists_local_json_file(self._cache_dir, json_hash):
+            return ipfs_jsu.load_local_json_file(self._cache_dir, json_hash)
+        json_dict = ipfs_jsu.load_json_object(json_hash, self._gateway_url)
+        ipfs_jsu.store_local_json_file(selff._cache_dir, json_hash, json_dict)
+        return json_dict
+
+    def store(self, json_dict: dict) -> str:
+        json_hash = ipfs_jsu.store_json_object(json_dict, self._rpc_api_url)
+        ipfs_jsu.store_local_json_file(self._cache_dir, json_hash, json_dict)
+        return json_hash
+
+    def exists(self, json_hash: str) -> bool:
+        return ipfs_jsu.exists_json_object(json_hash, self._gateway_url)
+
+    def compute_hash(self, json_dict: dict) -> str:
+        # TODO: A bit awkward to invoke an RPC endpoint to obtain the content identifier.
+        #       It would be better to accomplish this locally.
+        return ipfs_jsu.compute_hash(json_dict, self._rpc_api_url)
---
+++ src/jsontools/ipfs_storage_utils.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import json
+import requests
+import tempfile
+from .checksum import get_unique_json_repr
+from io import BytesIO
+
+
+def exists_local_json_file(filedir: Path, filename: str) -> bool:
+    filepath = Path(filedir) / filename
+    return filepath.is_file()
+
+
+def load_local_json_file(filedir: Path, filename: str) -> dict:
+    filepath = Path(filedir) / filename
+    with open(filepath, 'r') as f:
+        json_dict = json.load(f)
+    return json_dict
+
+
+def store_local_json_file(filedir: Path, filename: str, json_dict: dict):
+    jsonstr = get_unique_json_repr(json_dict)
+    filepath = Path(filedir) / filename
+    with open(filepath, 'w') as f:
+        f.write(jsonstr)
+
+
+def exists_json_object(json_hash: str, gateway_url: str) -> bool:
+    url = gateway_url.rstrip('/') + '/ipfs/' + json_hash
+    response = requests.head(url, allow_redirects=True)
+    return response.status_code == 200
+
+
+def load_json_object(json_hash: str, gateway_url:str) -> dict:
+    url = gateway_url.rstrip('/') + '/ipfs/' + json_hash
+    response = requests.get(url, stream=False)
+    if response.status_code != 200:
+        raise Exception(f'failed to fetch CID {json_hash}: HTTP {response.status_code}')
+    return response.text
+
+
+def _store_json_object(json_dict: dict, rpc_api_url: str, only_hash: bool=False) -> str:
+    jsonstr = get_unique_json_repr(json_dict)
+    json_bytes = jsonstr.encode('utf-8')
+    file_obj = BytesIO(json_bytes)
+    files = {'file': ('dummy', file_obj)}
+    if not only_hash:
+        upload_url = rpc_api_url.rstrip('/') + '/ipfs/upload'
+    else:
+        upload_url = rpc_api_url.rstrip('/') + '/ipfs/get-content-identifier'
+    response = requests.post(upload_url, files=files)
+    if response.status_code != 200:
+        raise Exception(f'Upload failed: HTTP {response.status_code} - {response.text}')
+    return response.json()['content_identifier']
+
+
+def store_json_object(json_dict: dict, rpc_api_url: str):
+    return _store_json_object(json_dict, rpc_api_url, only_hash=False)
+
+
+def compute_hash(json_dict: dict, rpc_api_url: str):
+    return _store_json_object(json_dict, rpc_api_url, only_hash=True)
